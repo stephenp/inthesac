10a7d8506dff34e8bb3e6b440ac4df62459e0dec
Justin Jones <justin@jstnjns.com>
2015-03-26 22:54:51 -0700

    Info logic, added blank thanks

--- src/scripts/controllers/info.js
+++ src/scripts/controllers/info.js
@@ -1,16 +1,24 @@
 'use strict';
 
 angular.module('inthesacApp')
-  .controller('InfoCtrl', function ($scope) {
+  .controller('InfoCtrl', function ($scope, $http, $location) {
 
     var init = function() {
           $scope.title = 'Information';
           $scope.click = click;
-          $scope.state = 'age'
+          $scope.state = 'age';
         },
 
         click = function() {
+          $scope.sending = true;
 
+          $http.post('https://formkeep.com/f/7d93b1dbfa4f', {
+            age: $scope.age,
+            gender: $scope.gender,
+            email: $scope.email
+          }).success(function() {
+            $location.path('/thanks')
+          });
         };
 
     init();

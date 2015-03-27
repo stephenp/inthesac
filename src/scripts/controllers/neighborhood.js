2d9bc1717f4837d2b84f2a6c083857d69cfdb5fa
Justin Jones <justin@jstnjns.com>
2015-03-26 19:14:30 -0700

    Working on form submission actions

--- src/scripts/controllers/neighborhood.js
+++ src/scripts/controllers/neighborhood.js
@@ -3,15 +3,37 @@
 angular.module('inthesacApp')
   .controller('NeighborhoodCtrl', function ($scope) {
 
-    $scope.title = 'Neighborhood';
+    var url = 'https://formkeep.com/f/7d97bd0b7130',
 
-    $scope.action = 'https://formkeep.com/f/7d97bd0b7130';
+        init = function() {
+          initValues();
+          bindActions();
+        },
 
-    $scope.options = [
-      'Old Sac',
-      'Downtown Sac',
-      'Midtown Sac',
-      'R-street Corridor'
-    ];
+        initValues = function() {
+          $scope.title = 'Neighborhood';
+          $scope.confirm = 'Confirm';
+
+          $scope.options = [
+            'Old Sac',
+            'Downtown Sac',
+            'Midtown Sac',
+            'R-street Corridor'
+          ];
+
+          $scope.selected = null;
+        },
+
+        bindActions = function() {
+          $scope.click = function() {
+            console.log($scope.selected);
+          };
+
+          $scope.disabled = function() {
+            return true;
+          };
+        };
+
+    init();
 
   });
